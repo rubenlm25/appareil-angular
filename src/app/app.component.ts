60d4fb56f2ba315bf62bc932425e8b6cd253b8a8
Ruben Lomo Maestu <ruben.lomomaestu@gmail.com>
2020-11-03 14:50:19 +0100

    first part finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mon-projet-angular';
+  isAuth = false;
+  lastUpdate = new Promise((resolve, reject) => {
+    const date = new Date();
+    setTimeout(
+      () => {
+        resolve(date);
+      }, 2000
+    );
+  });
+
+  appareils = [
+    {
+      name : 'Machine à laver',
+      status : 'éteint'
+    },
+    {
+      name: 'Frigo',
+      status: 'allumé'
+    },
+    {
+      name: 'Ordinateur',
+      status: 'éteint'
+    }
+  ];
+
+  constructor() {
+    setTimeout(
+      () => {
+        this.isAuth = true;
+      }, 4000
+    );
+  }
+  onAllumer(): void {
+    console.log('on allume tout !');
+
+  }
 }
